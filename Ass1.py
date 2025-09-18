f955d6129ace49899bb7ec06e41776775b69d24c
dhwanibachhao <150254971+dhwanibachhao@users.noreply.github.com>
2025-09-18 23:23:38 +0530

    Update Ass1.py

--- Ass1.py
+++ Ass1.py
@@ -1,21 +1,24 @@
-# a1=float(input("Enter the First number:"))
-# a2=float(input("Enter the Second number:"))
+#Task 1
+a1=float(input("Enter the First number:"))
+ a2=float(input("Enter the Second number:"))
 
-# add=a1+a2
-# sub=a1-a2
-# mul=a1*a2
-# div=a1/a2
+add=a1+a2
+sub=a1-a2
+mul=a1*a2
+div=a1/a2
 
-# print("Addition is:",add)
-# print("Substraction is:",sub)
-# print("Multiplication is:",mul)
-# print("Division is:",div)
+print("Addition is:",add)
+print("Substraction is:",sub)
+print("Multiplication is:",mul)
+print("Division is:",div)
 
 
+#Task 2
 a=input("Enetr your fisrt name:")
 b=input("Enter your last name:")
 c=a+b
 print(c)
 
 d=("Hello, " + c + "!" + " Welcome" + " to" + " the" + " Python" + " Program.")
+
 print(d)
