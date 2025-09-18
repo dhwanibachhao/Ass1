2c9d96ec992111091d167de719b70dc55b983744
dhwanibachhao <150254971+dhwanibachhao@users.noreply.github.com>
2025-09-18 23:19:30 +0530

    Add files via upload

---
+++ Ass1.py
@@ -0,0 +1,21 @@
+# a1=float(input("Enter the First number:"))
+# a2=float(input("Enter the Second number:"))
+
+# add=a1+a2
+# sub=a1-a2
+# mul=a1*a2
+# div=a1/a2
+
+# print("Addition is:",add)
+# print("Substraction is:",sub)
+# print("Multiplication is:",mul)
+# print("Division is:",div)
+
+
+a=input("Enetr your fisrt name:")
+b=input("Enter your last name:")
+c=a+b
+print(c)
+
+d=("Hello, " + c + "!" + " Welcome" + " to" + " the" + " Python" + " Program.")
+print(d)
